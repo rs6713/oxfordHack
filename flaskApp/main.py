0826d591bf6b0535cc5d7894dc56fc2d5e7784ce
Becks Simpson <rebecca.simpson13@imperial.ac.uk>
2017-11-25 21:35:57 +0000

    Working tweet language extraction

--- flaskApp/main.py
+++ flaskApp/main.py
@@ -3,8 +3,19 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from flask_assets import Environment, Bundle
 import tweepy
 
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
+
+
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '045f300f774941f986881338f7b42196',
+}
+noTweets=5
 
 @app.route('/')
 def main_page():
@@ -17,13 +28,19 @@ def main_page():
     api = tweepy.API(auth)
 
     #how to you compare to friends api.friends
-    user= "hacksmiths"#"BecksTSimpson"
-    public_tweets = api.user_timeline(id=user,count=50)
+    user= "BecksTSimpson" #"hacksmiths"#
+    public_tweets = api.user_timeline(id=user,count=noTweets)
     
     #print str(public_tweets[0])
-    
+    tweetInformation=[]
+
     for i, tweet in enumerate(public_tweets):
-        tw=str(tweet.text)
+        tweetInformation.append([])
+        tw=str(tweet.text) #created_at
+        tweetInformation[i].append(str(tweet.created_at))
+        tweetInformation[i].append(str(tweet.text))
+        
+        #print(tweet)
         #print (tw[0:2])
         if(tw[0:2]== "RT"):
             #print( "RETWEET")
@@ -31,16 +48,37 @@ def main_page():
         
         words= tw.split(" ")
         toremove=[]
-        for i,word in enumerate(words):
+        for u,word in enumerate(words):
             #print (str(word))
             if "https" in word or "#" in word or "@" in word:
-                words[i]=""
+                words[u]=""
             
-        print (" ".join(w for w in filter(lambda x: x!="", words)))
+        twitterPost= " ".join(w for w in filter(lambda x: x!="", words))
+        print("twitterPost:  ", twitterPost)
+    #wINDOWS LANGUAGE DETECT
+    #https://[location].api.cognitive.microsoft.com/text/analytics/v2.0/languages[?numberOfLanguagesToDetect]
 
-        
+
+
+        body={ "documents": [{ "id": "string","text":"hello"}]}
+        data_json = json.dumps(body)    
+        try:
+            conn = http.client.HTTPSConnection('northeurope.api.cognitive.microsoft.com')
+            conn.request("POST", "/text/analytics/v2.0/languages", data_json, headers)
+            response = conn.getresponse()
+            data = response.read()
+            data= json.loads(data)
+            #print(data)
+            langType=data["documents"][0]["detectedLanguages"][0]["name"] #langage type
+            tweetInformation[i].append(langType)
+            conn.close()
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+    print (tweetInformation)
 
     #for tweet in public_tweets:
     #    print (tweet.text)
-    return render_template('/main.html')
+    return render_template('/main.html', tweetData=tweetInformation)
   
