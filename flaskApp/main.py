b5edda995c6b6d6253362fba72e740ae56f07138
Becks Simpson <rebecca.simpson13@imperial.ac.uk>
2017-11-25 23:45:08 +0000

    lots of changes and dont want to lose please

--- flaskApp/main.py
+++ flaskApp/main.py
@@ -5,7 +5,12 @@ import tweepy
 
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 import json
+from flask import Markup
 
+        #data created
+        #twitterpost text
+        #language
+        #spelling score
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
@@ -35,10 +40,10 @@ def main_page():
     tweetInformation=[]
 
     for i, tweet in enumerate(public_tweets):
-        tweetInformation.append([])
+        
         tw=str(tweet.text) #created_at
-        tweetInformation[i].append(str(tweet.created_at))
-        tweetInformation[i].append(str(tweet.text))
+        
+        
         
         #print(tweet)
         #print (tw[0:2])
@@ -54,13 +59,20 @@ def main_page():
                 words[u]=""
             
         twitterPost= " ".join(w for w in filter(lambda x: x!="", words))
-        print("twitterPost:  ", twitterPost)
+        #print("twitterPost:  ", twitterPost)
+        tweetInformation.append([])
+        index= len(tweetInformation)-1
+
+
+
+        tweetInformation[index].append(str(tweet.created_at))
+        tweetInformation[index].append(twitterPost)
     #wINDOWS LANGUAGE DETECT
     #https://[location].api.cognitive.microsoft.com/text/analytics/v2.0/languages[?numberOfLanguagesToDetect]
 
 
-
-        body={ "documents": [{ "id": "string","text":"hello"}]}
+        #Get sentence language
+        body={ "documents": [{ "id": "string","text":twitterPost}]}
         data_json = json.dumps(body)    
         try:
             conn = http.client.HTTPSConnection('northeurope.api.cognitive.microsoft.com')
@@ -70,15 +82,45 @@ def main_page():
             data= json.loads(data)
             #print(data)
             langType=data["documents"][0]["detectedLanguages"][0]["name"] #langage type
-            tweetInformation[i].append(langType)
+            tweetInformation[index].append(langType)
             conn.close()
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+        #Get spelling score
+        wordcount = len(list(filter(lambda x: x!="", words)))
+        print(wordcount)
+        params = {'mkt': 'en-US', 'mode': 'spell', 'text': twitterPost}
+        key = '32ed427d493d4b3da7805d4cb9626855'
+        host = 'api.cognitive.microsoft.com'
+        path = '/bing/v7.0/spellcheck'
+        headers2 = {'Ocp-Apim-Subscription-Key': key,
+        'Content-Type': 'application/x-www-form-urlencoded'}
+        conn2 = http.client.HTTPSConnection(host)
+        params = urllib.parse.urlencode (params)
+        conn2.request ("POST", path, params, headers2)
+        response2 = conn2.getresponse()
+        print(response2)
+        print(response2.read)
+        response2 = response2.read()
+        response2 = json.loads(response2)
+        conn2.close()
+        if "flaggedTokens" in response2:
+            print(response2["flaggedTokens"])
+            tokens = len(response2["flaggedTokens"])
+            score = (int(wordcount) - int(tokens))/int(wordcount)
+        else:
+            score= 1
+        print("The number of correctly spelt words is: " + str(score) + " out of " + str(wordcount) +"!")
+        tweetInformation[index].append(score)
 
 
     print (tweetInformation)
 
     #for tweet in public_tweets:
     #    print (tweet.text)
-    return render_template('/main.html', tweetData=tweetInformation)
+    return render_template('/main.html', spellings= [x[3] for x in tweetInformation], dates= [x[0] for x in tweetInformation], posts= [x[1] for x in tweetInformation], languages= [x[2] for x in tweetInformation])
+
+if __name__ == "__main__":
+    app.run(port=8000)
   
